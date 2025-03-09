9e725259ff6643f761a7cdd074ca303887d0dcfc
Allan Maciel Gomes Galvão <allangg40@gmail.com>
2025-03-09 23:56:04 +0000

    finaliza o  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version");
+  const postgresMaxConnection = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const postgresOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int total_conexoes FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updatedAt,
+    postgres_version: postgresVersion.rows[0].server_version,
+    postgres_max_connection: parseInt(
+      postgresMaxConnection.rows[0].max_connections,
+    ),
+    postgres_opened_connections:
+      postgresOpenedConnections.rows[0].total_conexoes,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
-  });
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  console.log(responseBody);
+
+  const parseUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.postgres_version).toBe("16.0");
+  expect(responseBody.postgres_max_connection).toBe(100);
+  expect(responseBody.postgres_opened_connections).toBe(1);
+});
